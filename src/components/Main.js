1d5388760a85e6747d8f26399c6ad46bf40a2129
Jocelyn Chang <jocelync1094@gmail.com>
2022-04-20 10:48:34 -0700

    added local files to github

---
+++ src/components/Main.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import { Row, Col } from 'antd';
+import axios from 'axios';
+import SatSetting from './SatSetting';
+import SatelliteList from './SatelliteList';
+import {NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY} from "../constants";
+import WorldMap from './WorldMap';
+
+class Main extends Component {
+    constructor(){
+        super();
+        this.state = {
+            satInfo: null,
+            isLoadingList: false
+        };
+    }
+    showNearbySatellite = (setting) => {
+        this.fetchSatellite(setting);
+    }
+
+    fetchSatellite= (setting) => {
+        const {latitude, longitude, elevation, altitude} = setting;
+        const url = `/api/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
+
+   this.setState({
+       isLoadingList: true
+   });
+
+   axios.get(url)
+       .then(response => {
+           console.log(response.data)
+           this.setState({
+               satInfo: response.data,
+               isLoadingList: false
+           })
+       })
+       .catch(error => {
+           console.log('err in fetch satellite -> ', error);
+       })
+    }
+
+    showMap = () => {
+        console.log('show on the map');
+    }
+
+    render() {
+        const { satInfo } = this.state;
+        return (
+            <Row className="main">
+              <Col span={8} className="left-side">
+                <SatSetting onShow={this.showNearbySatellite} />
+                <SatelliteList
+                   satInfo={satInfo}
+                   isLoad={this.state.isLoadingList}
+                   onShowMap={this.showMap}
+                />
+              </Col>
+              <Col span={16} className="right-side">
+                <WorldMap />
+              </Col>
+            </Row>
+        );
+    }
+}
+
+export default Main;
